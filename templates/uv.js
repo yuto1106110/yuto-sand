5b0824e24356be89edaef73f9aa4abc2b555664e
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-09 22:17:33 +0900

    Update uv.js

--- templates/uv.js
+++ templates/uv.js
@@ -1,6 +1,6 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
-importScripts('/static/uv/uv.sw.js');
+importScripts('/templates/uv/bundle.js');
+importScripts('/templates/uv/config.js');
+importScripts('/templates/uv/sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
