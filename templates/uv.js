f8c57d90c6529714f783a25ad2807fb49331f49f
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-09 22:34:43 +0900

    Delete templates/uv.js

--- templates/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('/templates/uv/bundle.js');
-importScripts('/templates/uv/config.js');
-importScripts('/templates/uv/sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
