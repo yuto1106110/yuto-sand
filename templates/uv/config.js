3d5576e566bebc0183545e39dbfd594c7886a737
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-09 22:11:54 +0900

    Update config.js

--- templates/uv/config.js
+++ templates/uv/config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/templates/uv/uv.handler.js',
-    bundle: '/templates/uv/uv.bundle.js',
-    config: '/templates/uv/uv.config.js',
-    sw: '/templates/uv/uv.sw.js',
+    handler: '/templates/uv/handler.js',
+    bundle: '/templates/uv/bundle.js',
+    config: '/templates/uv/config.js',
+    sw: '/templates/uv/sw.js',
 };
