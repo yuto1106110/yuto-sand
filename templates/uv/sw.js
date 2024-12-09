9a8ec0ca73703e50562ca625c7db225e6abdb5ed
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-09 22:13:09 +0900

    Update sw.js

--- templates/uv/sw.js
+++ templates/uv/sw.js
@@ -1,5 +1,5 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
+importScripts('/templates/uv/bundle.js');
+importScripts('/templates/uv/config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
