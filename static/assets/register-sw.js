8d34ae063c778db7da6cbec72db719c7f2fce286
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-13 22:41:50 +0900

    Update register-sw.js

--- static/assets/register-sw.js
+++ static/assets/register-sw.js
@@ -3,9 +3,10 @@
 const BARE_SERVER = "https://tomp.app"
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./uv-sw.js', {
+navigator.serviceWorker.register('./static/uv-sw.js', {
     scope: '/service/'
-  }).then(() => {
+});
+then(() => {
     console.log("Service worker registered successfully!")
   })
 
