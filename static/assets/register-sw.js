c968d58dd570a704ffa035de16dc4bbbfb18aee9
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-14 10:58:08 +0900

    Update register-sw.js

--- static/assets/register-sw.js
+++ static/assets/register-sw.js
@@ -1,13 +1,12 @@
-// The service worker registration script
-// This must run successfully before Ultraviolet is available to use
-const BARE_SERVER = "https://tomp.app"
+const BARE_SERVER = "https://tomp.app";
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./uv-sw.js', {
-    scope: '/service/'
-  }).then(() => {
-    console.log("Service worker registered successfully!")
-  })
-
-  // BareMux.SetTransport("BareMod.BareClient", BARE_SERVER);
+    navigator.serviceWorker.register('./static/uv.sw.js', { scope: './static/' })
+    .then(() => {
+        console.log("Service worker registered successfully!");
+        // BareMux.SetTransport("BareMod.BareClient", BARE_SERVER);
+    })
+    .catch(error => {
+        console.error("Service worker registration failed:", error);
+    });
 }
