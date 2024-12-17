12597e04f7c418abfc425a01029efa15efe6fbd5
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-17 18:09:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -543,6 +543,7 @@ from typing import Union
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.mount("/css", StaticFiles(directory="./css"), name="static")
 app.mount("/sand", StaticFiles(directory="./blog", html=True), name="static")
+app.mount("/youtube", StaticFiles(directory="./youtube", html=True), name="static")
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 from fastapi.templating import Jinja2Templates
