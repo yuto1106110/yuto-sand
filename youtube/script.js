a1e65a3fe5671368e2530d7b122fd7810fc92ffd
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-17 18:17:23 +0900

    Update script.js

--- youtube/script.js
+++ youtube/script.js
@@ -1,16 +1,14 @@
 <script>
-
-    $(".btn").click(function (){
-        var link =$(".link").val();
-        var format =$(".format").children("option:selected").val();
-        var src=""+link+"="+format+"";
-        // children("option:selected")
-        download(link,format);
-
+    $(".btn").click(function () {
+        var link = $(".link").val();
+        var format = $(".format").children("option:selected").val();
+        
+        // download関数を呼び出す
+        download(link, format);
     });
 
-    function download(link,format){
-        $('.button-container')
-            .html('<iframe style="height: 50px; border: none; overflow: hidden;" src="https://loader.to/api/button/?url='+ link+ '&f='+ format + '"></iframe>');
+    function download(link, format) {
+        // iframeを生成し、指定されたリンクとフォーマットを使用して埋め込む
+        $('.button-container').html('<iframe style="height: 50px; border: none; overflow: hidden;" src="https://loader.to/api/button/?url=' + encodeURIComponent(link) + '&f=' + encodeURIComponent(format) + '"></iframe>');
     }
 </script>
