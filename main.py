9a7eef273828d479e1a8d25af3f2782294396fb9
mino-hobby-pro <ino.haru.426@gmail.com>
2024-12-18 18:48:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ video_apis = [
     r"https://script.google.com/macros/s/AKfycbxYjOWULjin5kpp-NcjjjGujVX3wy1TEJVUR2AZtR6-5c_q7GBr1Nctl2_Kat4lSboD/exec?videoId=",
 ]
 
-max_time = 20  # 最大待機時間の設定
+max_time = 30  # 最大待機時間の設定
 max_api_wait_time = 5  # APIリクエストの最大待機時間
 
 class APItimeoutError(Exception):
